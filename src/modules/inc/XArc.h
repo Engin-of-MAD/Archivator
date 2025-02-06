22537cffadba49aa2294c811eafac20d192a0bb7
2DRoach <darkman8350@gmail.com>
2025-02-07 02:22:00 +0300

    added of base parser cli

--- src/modules/inc/XArc.h
+++ src/modules/inc/XArc.h
@@ -8,6 +8,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <regex>
 #include <getopt.h>
 /**@brief
  *
@@ -16,8 +17,10 @@ class Settings
 {
 public:
 
-    enum Mode {Info = 0, Compress, Extractc, CompressWithCrypt} modeXArc;
-
+    enum Mode {Compress = 0, Extractc, Append, Update, Test, Info, Err} modeXArc;
+    enum ErrorsTypes{AbsenceInputsFiles = 0, ConflictsModesCXAUT,
+        SpecificMode, IncorrectCompressionLevel, AbsenceMods,
+        ErrCombinateMods, UnsupportedType, InvalidFormat,UnsupportedMethod, Ok};
     Settings();
     Settings(int argc, char *argv[]);
 private:
@@ -34,8 +37,17 @@ private:
     int compressionLevel;
     int currMode;
     void parse(int argc, char* argv[]);
-    void argsValidator(int arg);
+    ErrorsTypes parseHelp();
+    ErrorsTypes notSelectedMode();
+    ErrorsTypes selectMainMode();
+    ErrorsTypes conflictModes(char arg);
 
+    ErrorsTypes validatorOfModes(char arg);
+    ErrorsTypes validateArchiveType(const std::string& type);
+    ErrorsTypes validateLevelCompress(const std::string& level);
+    ErrorsTypes validateCompressionMethod(const std::string& method);
+    ErrorsTypes validateSplitSize(const std::string& size);
+    ErrorsTypes argsValidator(char arg);
 };
 
 std::wstring convertFromANSI(const char* str);
