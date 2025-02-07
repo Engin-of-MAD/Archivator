c042ab44d09ecb805bb36ac1c3601e2c3fcfcee6
2DRoach <darkman8350@gmail.com>
2025-02-07 15:35:59 +0300

    Added header with future interfaces for modules

---
+++ src/modules/inc/Interfaces.h
@@ -0,0 +1,13 @@
+//
+// Created by roach on 07.02.2025.
+//
+
+#ifndef INTERFACES_H
+#define INTERFACES_H
+
+class ICompressor{};
+class IFileIO{};
+class ILogger{};
+class IWorker{};
+
+#endif //INTERFACES_H
